6655822b8335b6dfee0141615df862a99e730e3c
Alex <alex96radu@protonmail.com>
2022-04-10 22:38:16 +0300

    test

--- dmenu/config.h
+++ dmenu/config.h
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#0ccc68", "#071a10" },
+	[SchemeSel] = { "#eeeeee", "#649568" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
